352a5493aec21853637a9b27d5694d25eca25d0b
Raúl Fabra <fabraraul@yahoo.com>
2024-05-01 02:36:55 +0200

    Finalizada la Parte Primera de la prueba Tecnica

--- unit-converter/src/components/Saved.jsx
+++ unit-converter/src/components/Saved.jsx
@@ -1,15 +1,25 @@
 import { useEffect, useState } from 'react'
+import { iconCross } from '../assets/icons'
 
 export function Saved ({ e }) {
   const [records, setRecords] = useState([])
 
+  const handleDelete = (e) => {
+    const div = e.target.closest('div')
+    div.remove()
+  }
+
   useEffect(() => {
     if (e.quantity === undefined) return
-
-    const newRecord = (<p>{e.quantity}{e.unity}<span> → </span>{e.result}{e.unityTransform}</p>)
-
+    const newRecord = (
+      <>
+        <p>{e.quantity}{e.unity}<span> → </span>{e.result}{e.unityTransform}</p>
+        <button onClick={handleDelete}>{iconCross()}</button>
+      </>
+    )
     setRecords([...records, newRecord])
   }, [e])
+
   return (
     <section>
       <h4>saved</h4>
